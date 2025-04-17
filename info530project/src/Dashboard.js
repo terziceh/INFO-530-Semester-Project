20b0dc4058e733ac9a68e35df1312a3f407d140e
pranavshp <udayakumarp2@vcu.edu>
2025-04-17 00:57:55 -0400

    React login page and dashboard screen

---
+++ info530project/src/Dashboard.js
@@ -0,0 +1,13 @@
+// src/Dashboard.js
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard</h1>
+      {/* Add more dashboard content here */}
+    </div>
+  );
+}
+
+export default Dashboard;
